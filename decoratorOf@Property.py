95e977d75ed47f4d431b5ca9c853aa813a2129a9
叶忠澈 <18107153152@163.com>
2017-05-24 10:31:56 +0800

    Add files via upload
    use @property to check args

---
+++ decoratorOf@Property.py
@@ -0,0 +1,33 @@
+class Student(object):
+
+    @property
+    def score(self):
+        return self._score
+
+    @score.setter
+    def score(self, value):
+        if not isinstance(value, int):
+            raise ValueError('score must be an integer!')
+        if value < 0 or value > 100:
+            raise ValueError('score must between 0 ~ 100!')
+        self._score = value
+# 定义只读属性，只定义getter方法，不定义setter方法就是一个只读属性：
+class Student(object):
+
+    @property
+    def birth(self):
+        return self._birth
+
+    @birth.setter
+    def birth(self, value):
+        self._birth = value
+
+    @property
+    def age(self):
+        return 2017 - self._birth
+s = Student()
+s.birth = 1993	
+print(s.age)  
+	
+
+
